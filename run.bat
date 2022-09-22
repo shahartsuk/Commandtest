ea8d64e70260c7856df0b2fe4a4a6f88fcb21553
shahar tsuk <shahartzuk23@gmail.com>
2022-09-22 13:50:27 +0300

    commandtest

---
+++ run.bat
@@ -0,0 +1,16 @@
+md 123TEST
+cd 123TEST
+type nul>run.txt
+md AAA
+md BBB
+md CCC
+cd BBB
+md DDD
+cd ..
+cd CCC
+cd/
+cd Windows
+dir>C:\123TEST\CCC\list.txt
+DEL /S /Q 123TEST
+RD /S /Q C:\123TEST
+echo haim please give me 100
